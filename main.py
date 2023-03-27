e8cbff5f7af68ae88539ec97881635fdf2c835bb
skacholia <74026955+skacholia@users.noreply.github.com>
2023-03-27 16:27:44 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import streamlit as st
 import os.path
 import pathlib
 import pandas as pd
+from langchain.agents import create_csv_agent
+
 
 st.write("""
 # GPT Data Analyst
@@ -9,5 +11,6 @@ st.write("""
 file = st.file_uploader("Choose a CSV file")
 
 if file is not None:
-    df= pd.read_csv(file)
-    st.write(df)
+    agent = create_csv_agent(OpenAI(temperature=0), file, verbose=True, max_iterations = 4)
+    q = st.text_input('Ask a question', 'Describe the dataset')
+    st.write(agent.run(q))
