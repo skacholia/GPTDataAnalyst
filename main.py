7c5a745fed1668eda84d53a8ee22d68d2ea5b574
skacholia <74026955+skacholia@users.noreply.github.com>
2023-03-27 16:30:42 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ file = st.file_uploader("Choose a CSV file")
 if file is not None:
     agent = create_csv_agent(OpenAI(temperature=0), file, verbose=True, max_iterations = 4)
     q = st.text_input('Ask a question', 'Describe the dataset')
-    st.write(agent.run(q))
+    st.write(agent.run(q + "Include numbers and interpret the data."))
